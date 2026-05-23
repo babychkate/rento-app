ec71af155d939f65bf6a065cc5eecb734ce05cea
kate_babych <Babych_k@i.ua>
2026-05-23 15:09:30 +0300

    fix icons size

--- src/components/StepIndicator/StepIndicator.jsx
+++ src/components/StepIndicator/StepIndicator.jsx
@@ -12,7 +12,7 @@ const StepIndicator = ({ step = 1, position = 'absolute' }) => {
     <div
       className={[
         posClass,
-        'w-19.5 h-19.5 rounded-full',
+        'w-22.5 h-22.5 rounded-full',
         'bg-[radial-gradient(circle_at_top_left,#cfd6ff_0%,#b8c2f3_45%,#aab5eb_100%)]',
         'border-[0.2px] border-white/80',
         'shadow-[inset_1px_1px_3px_rgba(255,255,255,1),inset_-2px_-3px_6px_rgba(0,30,120,0.15),0px_6px_12px_rgba(0,20,90,0.15)]',
