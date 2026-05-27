b4e87dc2127f83e42a2ad7fb6a252de9985ff9b2
kate_babych <Babych_k@i.ua>
2026-05-27 20:15:02 +0300

    FIX: оновила іконки в AccommodationDetailScreen

--- src/screens/AccommodationDetail/AccommodationDetailScreen.jsx
+++ src/screens/AccommodationDetail/AccommodationDetailScreen.jsx
@@ -8,7 +8,10 @@ import LandlordScreen from '../Landlord/LandlordScreen';
 import VerificationScreen from '../Verification/VerificationScreen';
 import ContractScreen from '../Contract/ContractScreen';
 import SecurityScreen from '../Security/SecurityScreen';
-import { BackIcon, StarIcon, ArrowIcon, LocationIcon, PhotoTourIcon } from '../../components/Icons/Icons';
+import {
+  BackIcon, StarIcon, ArrowIcon, LocationIcon,
+  PhotoTourIcon, VerifiedIcon, ShieldIcon, DocumentIcon
+} from '../../components/Icons/Icons';
 
 const AmenityIcon = ({ name }) => {
   const icons = {
@@ -239,14 +242,7 @@ if (screen === 'landlord') {
           {
             key: 'identity',
             label: 'Особу підтверджено',
-            icon: (
-              <svg width="32" height="32" viewBox="0 0 42 42" fill="none">
-                <path d="M35.2912 9.97429C34.8714 9.45643 34.1227 8.6615 33.6489 8.19351C30.3979 4.98235 25.9305 3 21 3C11.0589 3 3 11.0589 3 21C3 30.9411 11.0589 39 21 39C30.9411 39 39 30.9411 39 21C39 18.8672 38.6291 16.821 37.9482 14.9225"
-                  stroke="#0052FF" strokeWidth="2" strokeLinecap="round"/>
-                <path d="M13.1514 22.353L16.7953 27.0018C17.1654 27.474 17.8651 27.5162 18.2894 27.092L35.292 10.0894"
-                  stroke="#0052FF" strokeWidth="2" strokeLinecap="round"/>
-              </svg>
-            ),
+            icon: <VerifiedIcon />,
           },
           {
             key: 'security',
@@ -256,24 +252,12 @@ if (screen === 'landlord') {
                 {' '}розробив комплексну безпеку
               </>
             ),
-            icon: (
-              <svg width="32" height="32" viewBox="0 0 42 42" fill="none">
-                <path d="M37.824 8.71191L21.6457 2.14918C21.4095 2.05336 21.1456 2.05136 20.908 2.14359L4.72551 8.4243C4.29883 8.5899 4.03904 9.02404 4.09477 9.47833L6.15839 26.2978C6.35645 27.912 7.20129 29.3767 8.49943 30.3564L20.6756 39.5454C21.0321 39.8145 21.5238 39.8145 21.8803 39.5454L34.0565 30.3564C35.3546 29.3767 36.1995 27.912 36.3975 26.2978L37.796 14.8997"
-                  stroke="#0052FF" strokeWidth="2" strokeLinecap="round"/>
-                <path d="M12.3701 22.7123L16.1516 27.0389C16.5183 27.4585 17.1573 27.4974 17.5722 27.1253L38 8.80521"
-                  stroke="#0052FF" strokeWidth="2" strokeLinecap="round"/>
-              </svg>
-            ),
+            icon: <ShieldIcon />,
           },
           {
             key: 'contract',
             label: 'Переглянути зразок договору',
-            icon: (
-              <svg width="32" height="32" viewBox="0 0 42 42" fill="none">
-                <path d="M25.3519 8.28473V10.8627C25.3519 12.4235 26.6051 13.6952 28.1658 13.7181L32.5752 13.7826L33.9055 13.7623C35.6946 13.7349 36.4193 11.446 34.972 10.3939L30.7734 6.65312L28.147 4.57735C27.2642 3.87965 26.1719 3.50011 25.0467 3.5001L17.0925 3.5L8.9804 3.5001C8.13168 3.50011 7.34132 3.93205 6.88297 4.64635C6.62527 5.04795 6.48828 5.51508 6.48828 5.99225V35.8445C6.48828 36.3068 6.60897 36.7611 6.83842 37.1625C7.31128 37.9896 8.191 38.5 9.14374 38.5H32.7037C33.554 38.5 34.3644 38.1392 34.9333 37.5072L35.0118 37.4201C35.5077 36.8692 35.7822 36.1541 35.7822 35.4129V26.8193V17.7017"
-                  stroke="#0052FF" strokeWidth="2" strokeLinecap="round"/>
-              </svg>
-            ),
+            icon: <DocumentIcon />,
           },
         ].map(({ key, label, icon }, idx, arr) => (
           <div key={key}>
