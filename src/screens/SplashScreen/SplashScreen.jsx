c159ecd0f90be46f5c5d6a6c32586e3f1c073346
kate_babych <Babych_k@i.ua>
2026-05-31 21:23:03 +0300

    change color on splash background

--- src/screens/SplashScreen/SplashScreen.jsx
+++ src/screens/SplashScreen/SplashScreen.jsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 const SplashScreen = ({ onFinish }) => {
   return (
     <div className="relative w-full h-full flex items-center justify-center overflow-hidden"
-      style={{ background: '#a0adfc' }}>
+      style={{ background: '#A0ADFD' }}>
       <video
         src="/Intro.mp4"
         autoPlay
